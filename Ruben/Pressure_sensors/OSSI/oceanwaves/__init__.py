c3c4d853818712a4de3ff36f0a4ff3fea17a241f
Rubenrosman <r.g.c.rosman@student.tudelft.nl>
2025-02-10 16:15:13 +0100

    OSSI_04 & RBR_05
    script for ossi 4 and rbr 5 frame 1 up to 23 dec 2024

--- Ruben/Pressure_sensors/OSSI/oceanwaves/__init__.py
+++
@@ -1,8 +0,0 @@
-# __init__.py
-import numpy as np
-
-from .several_functions import waveNumber_dispertionRelation_NewtonRhapson
-from .several_functions import easyplot
-from .several_functions import save_variables
-from .several_functions import read_variables
-from .several_functions import compute_variables as calculate_wave_propagation
